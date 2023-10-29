a0c8bdb80417cee9d32916cd294e4c4a6d4d97be
Uana <71698232+oana73@users.noreply.github.com>
2023-10-29 23:47:34 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,8 @@
 
 int main() {
 
-    //const char* culoare = "roz";
     Romb romb1(2, "roz");
-    
-    //Romb romb2(romb1);//copy constructor
-    
-    Romb romb2 = romb1;
+    Romb romb2 = romb1;//copy constructor
 
     std::cout << "Perimetrul rombului1 de culoare " << romb1.getCuloare() << " este: " << romb1.perimetru() << std::endl;
     std::cout << "Perimetrul rombului2 de culoare " << romb2.getCuloare() << " este: " << romb2.perimetru() << std::endl;
